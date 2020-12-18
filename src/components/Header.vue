0ce3ed025eca8a8194be2b1f44edf89ea3ada3bd
Hemanth <hemanthvnbr@gmail.com>
2020-12-18 21:39:20 +0530

    testing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@
     </template>
     <template slot="start">
       <b-navbar-item tag="router-link" :to="{ path: '/home' }">
-        Home
+        Status
       </b-navbar-item>
       <b-navbar-item tag="router-link" :to="{ path: '/dashboard' }">
         Dashboard
